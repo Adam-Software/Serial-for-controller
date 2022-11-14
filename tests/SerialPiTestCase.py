19a907791ebf2e46dbf80320674bf0236f631eb1
Alexey Nesterov <vertigra.developer@gmail.com>
2022-11-14 15:06:41 +1000

    Update: import module from directory

--- tests/SerialPiTestCase.py
+++ tests/SerialPiTestCase.py
@@ -1,4 +1,22 @@
+import sys
+import os
 import unittest
+
+# import module from dirrectory
+# if need test package comment 3 lines below
+
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+
 from Serial.serialPi import SerialU
 
 
@@ -9,6 +27,7 @@ class SerialPiTestCase(unittest.TestCase):
         serial2 = SerialU()
 
         self.assertEqual(serial1, serial2)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
